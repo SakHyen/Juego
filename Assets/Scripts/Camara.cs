ea48c7af8e20f5b8f071dfd8f80b87b36d12c0b3
Francisco Moreno Castillo <56913146+SakHyen@users.noreply.github.com>
2019-11-12 20:55:23 +0100

    Update Camara.cs

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -2,53 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Place the script in the Camera-Control group in the component menu
+// Coloca el script en el menu de la camara
 [AddComponentMenu(V)]
 
 public class Camara : MonoBehaviour
 {
     private const string V = "Camera-Control/Smooth Follow CSharp";
 
-    // The target we are following
+    // Indica el objeto a seguir
     public Transform target;
-    // The distance in the x-z plane to the target
+    // Elige la distancia del objetivo en el plano X-Y
     public float distance = 10.0f;
-    // the height we want the camera to be above the target
+    // Altura de la camara respecto al objeto.
     public float height = 5.0f;
-    // How much we 
+    // 
     public float heightDamping = 2.0f;
     public float rotationDamping = 3.0f;
 
     void LateUpdate()
     {
-        // Early out if we don't have a target
+        // Salida si no tenemos el objeto
         if (!target)
             return;
 
-        // Calculate the current rotation angles
+        // Calcula los angulos de rotación
         float wantedRotationAngle = target.eulerAngles.y;
         float wantedHeight = target.position.y + height;
         float currentRotationAngle = transform.eulerAngles.y;
         float currentHeight = transform.position.y;
 
-        // Damp the rotation around the y-axis
+        // Modera la rotacion en el eje Y
         currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
 
-        // Damp the height
+        // Modera la altura
         currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
-        // Convert the angle into a rotation
+        // Convierte un angulo en una rotación
         Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
 
-        // Set the position of the camera on the x-z plane to:
-        // distance meters behind the target
+        // Establece la posición de la cámara en el plano x-z a una distancia de metros detrás del objeto
         transform.position = target.position;
         transform.position -= currentRotation * Vector3.forward * distance;
 
-        // Set the height of the camera
+        // Establece la altura de la cámara
         transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
 
-        // Always look at the target
+        // Siempre mira el objeto
         transform.LookAt(target);
     }
 }
